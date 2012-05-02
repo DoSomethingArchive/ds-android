43385acceb1d51d0ba880c442c013be3ea975cf2
Jason Elmore <jason@bohemian.cc>
2012-05-02 18:31:34 -0500

    gallery

--- dosomething/src/org/dosomething/android/activities/CampaignHowTo.java
+++ dosomething/src/org/dosomething/android/activities/CampaignHowTo.java
@@ -2,15 +2,33 @@ package org.dosomething.android.activities;
 
 import org.dosomething.android.R;
 
-import android.os.Bundle;
 import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.markupartist.android.widget.ActionBar;
 
 public class CampaignHowTo extends RoboActivity {
+	
+	private static final String HOW_TO = "how_to";
+	
+	@InjectView(R.id.actionbar) private ActionBar actionBar;
+	@InjectView(R.id.list) private ListView list;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.campaign_how_to);
+        
+        actionBar.setHomeAction(Campaigns.getHomeAction(this));
     }
+
+	public Intent getIntent(Context context){
+		Intent answer = new Intent(context, CampaignHowTo.class);
+		return answer;
+	}
 	
 }
