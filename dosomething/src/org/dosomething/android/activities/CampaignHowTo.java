77aad4fb6d8d01ee66fb55d1818bb0130b4b18bc
William Quast <will@bohemian.cc>
2012-05-03 18:47:08 -0500

    added how-to and faq lists

--- dosomething/src/org/dosomething/android/activities/CampaignHowTo.java
+++ dosomething/src/org/dosomething/android/activities/CampaignHowTo.java
@@ -1,19 +1,33 @@
 package org.dosomething.android.activities;
 
+import java.util.List;
+
 import org.dosomething.android.R;
+import org.dosomething.android.transfer.Campaign;
+import org.dosomething.android.transfer.HowTo;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.inject.Inject;
 import com.markupartist.android.widget.ActionBar;
 
 public class CampaignHowTo extends RoboActivity {
 	
 	private static final String CAMPAIGN = "campaign";
+	
+	@Inject private LayoutInflater inflater;
 	
 	@InjectView(R.id.actionbar) private ActionBar actionBar;
 	@InjectView(R.id.list) private ListView list;
@@ -24,6 +38,10 @@ public class CampaignHowTo extends RoboActivity {
         setContentView(R.layout.campaign_how_to);
         
         actionBar.setHomeAction(Campaigns.getHomeAction(this));
+        
+        Campaign campaign = (Campaign) getIntent().getExtras().get(CAMPAIGN);
+        
+        list.setAdapter(new MyAdapter(getApplicationContext(), campaign.getHowTos()));
     }
 	
 	public static Intent getIntent(Context context, org.dosomething.android.transfer.Campaign campaign){
@@ -32,4 +50,30 @@ public class CampaignHowTo extends RoboActivity {
 		return answer;
 	}
 	
+	
+	private class MyAdapter extends ArrayAdapter<HowTo> {
+
+		public MyAdapter(Context context, List<HowTo> objects){
+			super(context, android.R.layout.simple_list_item_1, objects);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+			if (v == null) {
+				v = inflater.inflate(R.layout.how_to_row, null);
+			}
+			
+			HowTo howTo = getItem(position);
+			
+			TextView header = (TextView)v.findViewById(R.id.header);
+			header.setText(howTo.getHeader());
+			
+			TextView body = (TextView)v.findViewById(R.id.body);
+			body.setText(howTo.getBody());
+
+			return v;
+		}
+		
+	}
 }
