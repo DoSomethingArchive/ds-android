44e164b89cb27d82bba0af8b09bcbd1269686a99
William Quast <will@bohemian.cc>
2012-05-11 16:15:47 -0500

    add Facebook login.

--- dosomething/src/org/dosomething/android/activities/Login.java
+++ dosomething/src/org/dosomething/android/activities/Login.java
@@ -11,19 +11,25 @@ import org.json.JSONObject;
 
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.android.DialogError;
 import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
 import com.google.inject.Inject;
 
 public class Login extends RoboActivity {
 	
+	private static final String TAG = "Login";
 	private static final int REQ_SIGN_UP = 111;
 	private static final int REQ_FACEBOOK_LOGIN = 111;
     
@@ -50,7 +56,7 @@ public class Login extends RoboActivity {
     	
     	//goToProfile();
     	
-    	new MyTask(email, password).execute();
+    	new MyLoginTask(email, password).execute();
     }
     
     private void goToProfile(){
@@ -64,29 +70,40 @@ public class Login extends RoboActivity {
     
     public void facebookLogin(View v){
 
-//    	facebook.authorize(this, new String[]{"birthday"}, REQ_FACEBOOK_LOGIN, new DialogListener(){
-//
-//			@Override
-//			public void onComplete(Bundle values) {
-//				Toast.makeText(getApplicationContext(), "complete", Toast.LENGTH_SHORT).show();
-//			}
-//
-//			@Override
-//			public void onFacebookError(FacebookError e) {
-//				Toast.makeText(getApplicationContext(), "facebook error", Toast.LENGTH_SHORT).show();
-//			}
-//
-//			@Override
-//			public void onError(DialogError e) {
-//				Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_SHORT).show();
-//			}
-//
-//			@Override
-//			public void onCancel() {
-//				Toast.makeText(getApplicationContext(), "cancel", Toast.LENGTH_SHORT).show();
-//			}
-//    		
-//    	});
+    	facebook.authorize(this, new String[]{"email","birthday"}, REQ_FACEBOOK_LOGIN, new DialogListener(){
+
+			@Override
+			public void onComplete(Bundle values) {
+				
+				new MyFacebookLoginTask(facebook.getAccessToken()).execute();
+			}
+
+			@Override
+			public void onFacebookError(FacebookError e) {
+				new AlertDialog.Builder(Login.this)
+					.setMessage(getString(R.string.facebook_auth_failed))
+					.setCancelable(false)
+					.setPositiveButton(getString(R.string.ok_upper), null)
+					.create()
+					.show();
+			}
+
+			@Override
+			public void onError(DialogError e) {
+				new AlertDialog.Builder(Login.this)
+					.setMessage(getString(R.string.facebook_auth_failed))
+					.setCancelable(false)
+					.setPositiveButton(getString(R.string.ok_upper), null)
+					.create()
+					.show();
+			}
+
+			@Override
+			public void onCancel() {
+				Log.d(TAG, "facebook authorize cancelled");
+			}
+    		
+    	});
     }
     
     @Override
@@ -103,7 +120,7 @@ public class Login extends RoboActivity {
         }
     }
     
-	private class MyTask extends AbstractWebserviceTask {
+	private class MyLoginTask extends AbstractWebserviceTask {
 
 		private String username;
 		private String password;
@@ -112,10 +129,86 @@ public class Login extends RoboActivity {
 		
 		private ProgressDialog pd;
 		
-		public MyTask(String username, String password) {
+		public MyLoginTask(String username, String password) {
 			super(sessionContext);
 			this.username = username;
 			this.password = password;
+		}
+		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pd = ProgressDialog.show(context, null, getString(R.string.logging_in));
+		}
+
+		@Override
+		protected void onSuccess() {
+			
+			if(loginSuccess) {
+				goToProfile();
+			} else {
+				Toast.makeText(Login.this, getString(R.string.log_in_auth_failed), Toast.LENGTH_LONG).show();
+			}
+		}
+		
+		@Override
+		protected void onFinish() {
+			pd.dismiss();
+		}
+
+		@Override
+		protected void onError() {
+			
+			new AlertDialog.Builder(Login.this)
+				.setMessage(getString(R.string.facebook_auth_failed))
+				.setCancelable(false)
+				.setPositiveButton(getString(R.string.ok_upper), null)
+				.create()
+				.show();
+		}
+
+		@Override
+		protected void doWebOperation() throws Exception {
+			String url = "http://www.dosomething.org/?q=rest/user/login.json";
+			
+			Map<String,String> params = new HashMap<String, String>();
+			params.put("username", username);
+			params.put("password", password);
+			
+			WebserviceResponse response = doPost(url, params);
+			
+//			CookieStore cookieStore = (CookieStore) sessionContext.getHttpContext().getAttribute(ClientContext.COOKIE_STORE);
+//			
+//			if(cookieStore.getCookies() != null){
+//				for(Cookie cookie : cookieStore.getCookies()){
+//					Log.e("ME", cookie.getName() + " : " + cookie.getValue());
+//				}
+//			}
+			
+			if(response.getStatusCode()>=400 && response.getStatusCode()<500) {
+				loginSuccess = false;
+			} else {
+				JSONObject obj = response.getBodyAsJSONObject();
+				JSONObject user = obj.getJSONObject("user");
+				String uid = user.getString("uid");
+				
+				new UserContext(getApplicationContext()).setLoggedIn(uid);
+				loginSuccess = true;
+			}
+		}
+	}
+	
+	private class MyFacebookLoginTask extends AbstractWebserviceTask {
+
+		private String accessToken;
+		
+		private boolean loginSuccess;
+		
+		private ProgressDialog pd;
+		
+		public MyFacebookLoginTask(String accessToken) {
+			super(sessionContext);
+			this.accessToken = accessToken;
 		}
 		
 		@Override
@@ -147,11 +240,10 @@ public class Login extends RoboActivity {
 
 		@Override
 		protected void doWebOperation() throws Exception {
-			String url = "http://www.dosomething.org/?q=rest/user/login.json";
+			String url = "http://www.dosomething.org/?q=rest/user/fblogin.json";
 			
 			Map<String,String> params = new HashMap<String, String>();
-			params.put("username", username);
-			params.put("password", password);
+			params.put("access_token", accessToken);
 			
 			WebserviceResponse response = doPost(url, params);
 			
