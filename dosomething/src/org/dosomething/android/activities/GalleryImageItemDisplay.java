44927b509be2a1675cdc738daad50793e4303c02
Jon Uy <jonxuy@gmail.com>
2012-06-06 10:31:31 -0400

    Gallery images open to new activity within app instead of sending user to external web browser

---
+++ dosomething/src/org/dosomething/android/activities/GalleryImageItemDisplay.java
@@ -0,0 +1,52 @@
+package org.dosomething.android.activities;
+
+import org.dosomething.android.R;
+import org.dosomething.android.widget.CustomActionBar;
+import org.dosomething.android.widget.ProgressBarImageLoadingListener;
+
+import roboguice.inject.InjectView;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.inject.Inject;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+/**
+ * Activity for displaying an image from a campaign's gallery 
+ */
+public class GalleryImageItemDisplay extends AbstractActivity {
+
+	private static final String GALLERY_IMG_URL = "gallery-img-url";
+	
+	@Inject LayoutInflater inflater;
+	@Inject private ImageLoader imageLoader;
+	@InjectView(R.id.actionbar) private CustomActionBar actionBar;
+	@InjectView(R.id.galleryImageItemDisplay) private ImageView imageView;
+	
+	@Override
+	protected String getPageName() {
+		return "campaign-gallery-image";
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gallery_image_fullscreen);
+		
+		actionBar.addAction(Campaigns.getHomeAction(this));
+
+		Bundle bundle = getIntent().getExtras();
+		String imgurl = bundle.getString(GALLERY_IMG_URL);
+		if (imgurl != "") {
+	        imageLoader.displayImage(imgurl, imageView);
+			ProgressBar progressBar = (ProgressBar)findViewById(R.id.galleryImageItemDisplayProgress);
+			imageLoader.displayImage(imgurl, imageView, new ProgressBarImageLoadingListener(progressBar));
+		}
+		else {
+			Toast.makeText(this, R.string.gallery_image_item_display_error, Toast.LENGTH_LONG);
+		}
+	}
+}
