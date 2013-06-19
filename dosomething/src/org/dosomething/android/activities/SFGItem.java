09c13db283f9be2e64b429c27d9d8e4e331dcfb0
Jon Uy <jonxuy@gmail.com>
2013-06-19 14:59:12 -0400

    Facebook 'Share Me' functionality - TOOD: update share count on the server

--- dosomething/src/org/dosomething/android/activities/SFGItem.java
+++ dosomething/src/org/dosomething/android/activities/SFGItem.java
@@ -9,13 +9,21 @@ import org.dosomething.android.widget.ProgressBarImageLoadingListener;
 
 import roboguice.inject.InjectView;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.facebook.FacebookException;
+import com.facebook.widget.WebDialog;
+import com.facebook.widget.WebDialog.Builder;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -32,6 +40,7 @@ public class SFGItem extends AbstractActivity {
 	@InjectView(R.id.state) private TextView stateView;
 	@InjectView(R.id.story) private TextView storyView; 
 	@InjectView(R.id.progressBar) private ProgressBar progressBar;
+	@InjectView(R.id.shareButton) private Button shareButton;
 	
 	private Campaign campaign;
 	private SFGGalleryItem sfgItem;
@@ -52,6 +61,14 @@ public class SFGItem extends AbstractActivity {
 		}
 		
 		campaign = (Campaign)getIntent().getExtras().get(DSConstants.EXTRAS_KEY.CAMPAIGN.getValue());
+		
+		shareButton.setTypeface(dinTypeface);
+		shareButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				publishFeedDialog();
+			}
+		});
 	}
 	
 	@Override
@@ -84,6 +101,41 @@ public class SFGItem extends AbstractActivity {
 				storyView.setText(sfgItem.getStory());
 			}
 		}
+	}
+	
+	/**
+	 * Displaying the Facebook feed dialog with info about this submission
+	 */
+	private void publishFeedDialog() {
+	    Bundle params = new Bundle();
+	    params.putString("name", getString(R.string.campaign_sfg_p4p_share_title));
+	    params.putString("caption", "DoSomething.org - " + campaign.getName());
+	    params.putString("description", getString(R.string.campaign_sfg_p4p_share_description, sfgItem.getName()));
+	    params.putString("link", campaign.getSFGData().getGalleryUrl() + sfgItem.getId());
+	    params.putString("picture", campaign.getSFGData().getGalleryUrl() + sfgItem.getImageURL());
+
+	    Builder feedDialogBuilder = new WebDialog.Builder(SFGItem.this, DSConstants.FACEBOOK_APP_ID, "feed", params);
+	    WebDialog feedDialog = feedDialogBuilder.build();
+	    feedDialog.setOnCompleteListener(new WebDialog.OnCompleteListener() {
+			@Override
+			public void onComplete(Bundle values, FacebookException error) {
+				// TODO implement
+				boolean shareSuccess = values != null && !values.isEmpty();
+			}
+	    });
+	    feedDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+			@Override
+			public void onCancel(DialogInterface dialog) {
+				// TODO implement
+			}
+		});
+	    feedDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+			@Override
+			public void onDismiss(DialogInterface dialog) {
+				// TODO implement
+			}
+		});
+	    feedDialog.show();
 	}
 	
 	public static Intent getIntent(Context context, SFGGalleryItem item, Campaign camp) {
