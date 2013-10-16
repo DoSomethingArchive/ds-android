ad49f2e5f0ab686e914533ed3bd17f51fcba13ba
Jon Uy <jonxuy@gmail.com>
2013-10-16 17:30:00 -0400

    UA analytics setup for start and end of activities

--- dosomething/src/org/dosomething/android/MyApplication.java
+++ dosomething/src/org/dosomething/android/MyApplication.java
@@ -1,29 +1,30 @@
 package org.dosomething.android;
 
-import org.acra.ACRA;
-import org.acra.annotation.ReportsCrashes;
-
 import android.app.Application;
 
 import com.urbanairship.AirshipConfigOptions;
 import com.urbanairship.UAirship;
 import com.urbanairship.push.PushManager;
 
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
 @ReportsCrashes(formKey="dEhCTHFETWdJcUYxX0s0TUV0RzV0Ync6MQ")
 public class MyApplication extends Application {
 
-	@Override
-	public void onCreate() {
-		ACRA.init(this);
-		super.onCreate();
-		
-		AirshipConfigOptions uaOptions = AirshipConfigOptions.loadDefaultOptions(this);
-		uaOptions.inProduction = DSConstants.inProduction;
-		
-		UAirship.takeOff(this, uaOptions);
-		
-		// TODO: this should be configurable by the user
-		PushManager.enablePush();
-	}
+    @Override
+    public void onCreate() {
+        ACRA.init(this);
+        super.onCreate();
+
+        AirshipConfigOptions uaOptions = AirshipConfigOptions.loadDefaultOptions(this);
+        uaOptions.inProduction = DSConstants.inProduction;
+        uaOptions.analyticsEnabled = DSConstants.inProduction;
+
+        UAirship.takeOff(this, uaOptions);
+
+        // TODO: this should be configurable by the user
+        PushManager.enablePush();
+    }
 
 }
