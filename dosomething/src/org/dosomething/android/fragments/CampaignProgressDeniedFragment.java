9f592c2c9fba5dfdada253249deaaa4e9f6e2dc5
Jon Uy <jonxuy@gmail.com>
2013-12-20 17:40:33 -0500

    Puppet sloth takes over the sign up prompt fragment

--- dosomething/src/org/dosomething/android/fragments/CampaignProgressDeniedFragment.java
+++ dosomething/src/org/dosomething/android/fragments/CampaignProgressDeniedFragment.java
@@ -1,24 +1,41 @@
 package org.dosomething.android.fragments;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.dosomething.android.R;
 
 import roboguice.fragment.RoboFragment;
+import roboguice.inject.InjectView;
 
 /**
  * Fragment for the CampaignPagerAdapter to prompt user to sign up in order to progress
  * and participate in the campaign.
  */
 public class CampaignProgressDeniedFragment extends RoboFragment {
+
+    private Typeface mTypefaceLEngineer;
+
+    @InjectView(R.id.progress_denied_text)TextView mDeniedText;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
+        mTypefaceLEngineer = Typeface.createFromAsset(getActivity().getAssets(), "LEngineer-Regular.otf");
+
         View rootView = inflater.inflate(R.layout.fragment_campaign_progress_denied, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mDeniedText.setTypeface(mTypefaceLEngineer);
     }
 }
