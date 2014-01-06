f3a37fb763f91a959265ee8b14518fde834d18bd
Jon Uy <jonxuy@gmail.com>
2014-01-06 18:16:52 -0500

    OOM crash fix for Galaxy S3 and probably other devices with high screen density - images moved to drawable-xhdpi/ (which seems to help a little with the image scaling that happens, but still probably want to provide the appropriate resources for the diff densities) - Walkthrough fragments null out the image resources when fragment is stopped to hopefully allow the GC to free up that memory

--- dosomething/src/org/dosomething/android/fragments/WalkthroughFragment.java
+++ dosomething/src/org/dosomething/android/fragments/WalkthroughFragment.java
@@ -22,7 +22,14 @@ public class WalkthroughFragment extends RoboFragment {
 
     @Inject private @Named("ProximaNova-Reg")Typeface typefaceReg;
 
+    private ImageView mImageView;
+    private TextView mTextView;
+
     private int mPageNumber;
+
+    public WalkthroughFragment() {
+        mPageNumber = 0;
+    }
 
     public WalkthroughFragment(int pageNumber) {
         mPageNumber = pageNumber;
@@ -33,26 +40,46 @@ public class WalkthroughFragment extends RoboFragment {
         super.onCreateView(inflater, container, savedInstanceState);
 
         View rootView = inflater.inflate(R.layout.fragment_walkthrough, container, false);
-        ImageView imageView = (ImageView)rootView.findViewById(R.id.image);
-        TextView textView = (TextView)rootView.findViewById(R.id.text);
-        textView.setTypeface(typefaceReg);
+        mImageView = (ImageView)rootView.findViewById(R.id.image);
+        mTextView = (TextView)rootView.findViewById(R.id.text);
+        mTextView.setTypeface(typefaceReg);
 
-        // TODO: change out dummy images and text for finalized walkthrough content
         switch(mPageNumber) {
             case 0:
-                imageView.setImageResource(R.drawable.walkthrough_bg1);
-                textView.setText(R.string.walkthrough_screen1);
+                mTextView.setText(R.string.walkthrough_screen1);
                 break;
             case 1:
-                imageView.setImageResource(R.drawable.walkthrough_bg2);
-                textView.setText(R.string.walkthrough_screen2);
+                mTextView.setText(R.string.walkthrough_screen2);
                 break;
             case 2:
-                imageView.setImageResource(R.drawable.walkthrough_bg3);
-                textView.setText(R.string.walkthrough_screen3);
+                mTextView.setText(R.string.walkthrough_screen3);
                 break;
         }
 
         return rootView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        switch(mPageNumber) {
+            case 0:
+                mImageView.setImageResource(R.drawable.walkthrough_bg1);
+                break;
+            case 1:
+                mImageView.setImageResource(R.drawable.walkthrough_bg2);
+                break;
+            case 2:
+                mImageView.setImageResource(R.drawable.walkthrough_bg3);
+                break;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        mImageView.setImageDrawable(null);
     }
 }
